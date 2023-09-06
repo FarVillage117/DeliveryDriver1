698ff71bcf251cdf408b089803ceae51598aa8ca
FarVillage117 <aidanlongueville117@gmail.com>
2023-09-06 13:23:32 -0400

    Steer/Move

--- DeliveryDriver1_/Assets/Car.cs
+++ DeliveryDriver1_/Assets/Car.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float steerSpeed = 0.1f;
+    float moveSpeed = 0.1f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f)
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
