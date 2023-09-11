e23225d3e10ed157a3fb64117ae13d22ffb6fb90
FarVillage117 <aidanlongueville117@gmail.com>
2023-09-11 12:58:08 -0400

    onTrigger

--- DeliveryDriver1_/Assets/Car.cs
+++ DeliveryDriver1_/Assets/Car.cs
@@ -15,7 +15,7 @@ public class Car : MonoBehaviour
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, -moveAmount, 0);
     }
